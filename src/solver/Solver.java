a74b5f952836d962cfdd29b5f74f7dad88bf4081
Vlad Lastname <warangel.alk@gmail.com>
2013-10-14 19:09:21 +0300

    No code no problems

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -41,49 +41,6 @@ public class Solver {
 
 			solution[coords.a][coords.b] = dec;
 		}
-
-		// Нахождение оптимального плана методом потенциалов
-		Integer[] minePotential = new Integer[data.getMines().length];
-		Integer[] factoryPotential = new Integer[data.getFactories().length];
-		ArrayList<Pair> basisList = new ArrayList<Pair>(minePotential.length
-				+ factoryPotential.length - 1);
-		ArrayList<Pair> nobasisList = new ArrayList<Pair>(minePotential.length
-				* factoryPotential.length - basisList.size());
-		for(int i = 0; i < solution.length; ++i) {
-			for(int j = 0; j < solution[0].length; ++j) {
-				if(solution[i][j] != null)
-					basisList.add(new Pair(i, j));
-				else
-					nobasisList.add(new Pair(i, j));
-			}
-		}
-
-		Pair maxCost = maxCost();
-		minePotential[maxCost.a] = 0;
-		for(int i = 0; i < factoryPotential.length; ++i) {
-			if(basisList.contains(new Pair(maxCost.a, i)))
-				factoryPotential[i] = data.getCost()[maxCost.a][i];
-		}
-		
-		boolean repeat = false;
-		while(repeat) {
-			repeat = false;
-			for(Pair p : basisList) {
-				if(minePotential[p.a] != null) {
-					if(factoryPotential[p.b] != null) {
-						// Nothing
-					} else {
-						factoryPotential[p.b] = data.getCost()[p.a][p.b] - minePotential[p.a];
-					}
-				} else if (factoryPotential[p.b] != null) {
-					if(minePotential[p.a] != null) {
-						minePotential[p.a] = data.getCost()[p.a][p.b] - factoryPotential[p.b];
-					} else {
-						repeat = true;
-					}
-				}
-			}
-		}
 		
 		return solution;
 	}
@@ -109,32 +66,6 @@ public class Solver {
 							result.a = i;
 							result.b = j;
 						}
-					}
-				}
-			}
-		}
-
-		return result;
-	}
-	
-	/**
-	 * Максимальная стоимость (среди всех ячеек)
-	 * 
-	 * @return координаты ячейки с максимальной стоимостью
-	 */
-	private Pair maxCost() {
-		Pair result = new Pair(0, 0);
-		Integer[][] cost = data.getCost();
-
-		int max = Integer.MIN_VALUE;
-
-		for (int i = 0; i < cost.length; ++i) {
-			for (int j = 0; j < cost[0].length; ++j) {
-				if (cost[i][j] != null) {
-					if (cost[i][j] > max) {
-						max = cost[i][j];
-						result.a = i;
-						result.b = j;
 					}
 				}
 			}
